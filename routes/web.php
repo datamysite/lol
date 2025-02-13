7e45d64662dfe2b89b22745ba755bd5967808eef
Waseem <waseem@datamysite.com>
2025-02-13 12:54:15 +0400

    Initiate

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+//Migration
+Route::get('/migrate', function () {
+    Artisan::call('migrate');
+    //Artisan::call('migrate', ['--force' => true ]);
+    dd('migrated!');
+});
+
 Route::get('/', function () {
     return view('index');
 });
