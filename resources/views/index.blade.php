89bf71f9b17a938cc9450b3e4c085bcc5d98f9aa
Waseem <waseem@datamysite.com>
2025-02-13 12:45:51 +0400

    Initiate

---
+++ resources/views/index.blade.php
@@ -0,0 +1,307 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+        integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <!-- owl carousel css-->
+    <link rel="stylesheet" href="{{URL::to('/')}}/public/owl-carousel/assets/owl.carousel.min.css" type="text/css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{URL::to('/')}}/public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{URL::to('/')}}/public/css/style.css">
+
+    <title>Let`s off Leash</title>
+</head>
+
+<body>
+
+    <div id="fh5co-hero-carousel" class="carousel slide header" data-ride="carousel">
+        <nav class="navbar fixed-top navbar-expand-xl">
+            <div class="container">
+                <a class="navbar-brand mobile-logo" href="#">
+                    <img src="{{URL::to('/')}}/public/images/lol-logo.png" alt="LOL" height="50px" />
+                </a>
+                <button class="navbar-toggler" data-target="#my-nav" onclick="myFunction(this)" data-toggle="collapse">
+                    <span class="bar1"></span> <span class="bar2"></span> <span class="bar3"></span> </button>
+
+                <div id="my-nav" class="collapse navbar-collapse">
+
+                    <div>
+                        <p class="text-left follow text-theme">Follow Us:</p>
+                        <ul class="navbar-nav float-left social-links">
+                            <li class="nav-item">
+                                <a class="nav-link" href="https://www.youtube.com/@LetsOffLeash" target="_blank" class="text-theme"><i class="fab fa-youtube"></i></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="https://instagram.com/letsoffleash" target="_blank" class="text-theme"><i class="fab fa-instagram"></i></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="http://facebook.com/LetsOffLeashlol" target="_blank" class="text-theme"><i class="fab fa-facebook-f"></i></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="https://linkedin.com/company/letsoffleash" target="_blank" class="text-theme"><i class="fab fa-linkedin"></i></a>
+                            </li>
+
+                        </ul>
+                    </div>
+
+                    <ul class="navbar-nav mx-auto logo-desktop">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#"><img src="{{URL::to('/')}}/public/images/lol-logo.png" alt="Vista Pro" height="80px" /></a>
+                        </li>
+                    </ul>
+
+                    <ul class="navbar-nav float-right menu-links">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#about-us">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#content">Content</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#community">Community</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#contact">Contact</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+
+                <img class="d-block w-100 home-bg" alt="home-bg" src="{{URL::to('/')}}/public/images/home-bg.png">
+
+                <div class="carousel-caption d-md-block">
+                    <p class="frst-hrd text-theme">Freedom in Every Stories!</p>
+                    <h5 class="hero-title">Welcome to <br><font class="text-theme">LOL (Let's Off Leash)</font></h5>
+                    <p><small>Hosted by</small> <font class="text-theme"><strong>Kasturi Jha.</strong></font></p>
+
+                </div>
+
+
+            </div>
+            <div class="scroll-button">
+                <p>It’s a movement to explore unheard stories of <font class="text-theme">animals</font> and <font class="text-theme">people</font>.</p>
+                <a href="#about-us" class="page-scroll">
+                    <img src="{{URL::to('/')}}/public/images/arrows-down.png" alt="arrow down" />
+                </a>
+
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container-fluid fh5co-about-us" id="about-us">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+
+    
+
+                    <div class="owl-carousel owl-carousel1 owl-theme">
+                        <div>
+                            <img src="{{URL::to('/')}}/public/images/about3.png" alt="" class="img-fluid" />
+                        </div>
+                        <div>
+                            <img src="{{URL::to('/')}}/public/images/about2.png" alt="" class="img-fluid" />
+                        </div>
+                        <div>
+                            <img src="{{URL::to('/')}}/public/images/about4.png" alt="" class="img-fluid" />
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-8">
+                    <div class="about-us-content">
+                        <h2>ABOUT US</h2>
+                        <p>
+                            Welcome to LOL "Let's Off Leash" – where freedom meets storytelling! 
+                            <br>Founded and hosted by Kasturi Jha, "Let’s Off Leash" is more than just a podcast; it’s a movement dedicated to bringing unheard stories of animals and people to the forefront. With a global community of over 250K followers and 30K+ YouTube subscribers, we are committed to inspiring, educating, and transforming lives through powerful narratives. Featuring over 20 incredible guests, including veterinarians, animal rescuers, biologists, and storytellers, we explore the profound connections between humans and animals through heartwarming rescues, extraordinary human journeys, and insightful discussions.
+                            <br><br>
+                            Our storytelling approach follows a unique Problem = Insight = Solution framework, ensuring that each episode not only highlights challenges but also provides actionable solutions. We cover topics ranging from health and wellness to lifestyle and entertainment, addressing real-world issues with practical insights. LOL "Let's Off Leash" is your go-to destination for uplifting stories, meaningful discussions, and a deeper understanding of the bond between animals and people. Join us on this journey of exploration and inspiration, and let’s off leash together!
+                        </p>
+                        <a href="https://www.youtube.com/@LetsOffLeash" class="read-more" target="_blank">Visit Channel</a>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container-fluid our-content" id="content">
+        <div class="container">
+            <h2>OUR CONTENT</h2>
+            <div class="row">
+                <div class="col-md-12">
+                    <p>Our YouTube channel is a hub of engaging and thought-provoking content that brings stories to life through compelling visuals and in-depth storytelling. From exclusive interviews with experts and rescuers to heartwarming animal rescue stories and insightful discussions on human resilience, every video is crafted to educate, inspire, and entertain. Whether you're looking for eye-opening narratives or practical solutions to real-world issues, our channel offers a diverse range of content that connects, informs, and uplifts.</p>
+                    <br>
+                </div>
+                <div class="col-md-4">
+                    <iframe src="https://www.youtube.com/embed/K8Gu3k5vAx0?si=aDde_8vTwP2aM2-V" allowfullscreen></iframe>
+                    <h5 class="text-theme2">Salman Khan Exotic Pet Dog - <font class="text-theme text-bold">Kasturi Jha</font></h5>
+                </div>
+                <div class="col-md-4">
+                    <iframe src="https://www.youtube.com/embed/rL_3BLBIZko?si=WQZ3AM0Hu5gP-0jN" allowfullscreen></iframe>
+                    <h5 class="text-theme2">Must Do This Business in 2025 - <font class="text-theme text-bold">Kasturi Jha</font></h5>
+                </div>
+                <div class="col-md-4">
+                    <iframe src="https://www.youtube.com/embed/uvi-hqkNoPg?si=tgrmmFOpk_dNZ9JG" allowfullscreen></iframe>
+                    <h5 class="text-theme2">Must-Listen Podcast for Pet Parents - <font class="text-theme text-bold">Kasturi Jha</font></h5>
+                </div>
+                <div class="col-md-4">
+                    <iframe src="https://www.youtube.com/embed/uHx59MKFu6Y?si=kzBwqEFj66JYcu3y" allowfullscreen></iframe>
+                    <h5 class="text-theme2">Live Telepathic Conversations - <font class="text-theme text-bold">Kasturi Jha</font></h5>
+                </div>
+                <div class="col-md-4">
+                    <iframe src="https://www.youtube.com/embed/3zMfNM7EslE?si=TBa5XM8J2azg4-ij" allowfullscreen></iframe>
+                    <h5 class="text-theme2">Lebanon’s Animals Left to Suffer War - <font class="text-theme text-bold">Kasturi Jha</font></h5>
+                </div>
+                <div class="col-md-4">
+                    <iframe src="https://www.youtube.com/embed/RAAjkx7t6Pc?si=8opFPfpVQXr3IVDo" allowfullscreen></iframe>
+                    <h5 class="text-theme2">How Do Dogs See The World? - <font class="text-theme text-bold">Kasturi Jha</font></h5>
+                </div>
+                <div class="col-md-4">
+                    <iframe src="https://www.youtube.com/embed/pd1f8lYVYe4?si=a9mw-C5XA4o44OVW" allowfullscreen></iframe>
+                    <h5 class="text-theme2">Real Talk Dog vs Cat real fight to death - <font class="text-theme text-bold">Kasturi Jha</font></h5>
+                </div>
+                <div class="col-md-4">
+                    <iframe src="https://www.youtube.com/embed/KgItsWczMUo?si=bsKhfp28bbId3Uss" allowfullscreen></iframe>
+                    <h5 class="text-theme2">Pet Health Insurance: Veterinarian Answers Pet Questions - <font class="text-theme text-bold">Kasturi Jha</font></h5>
+                </div>
+                <div class="col-md-4">
+                    <iframe src="https://www.youtube.com/embed/CZdKINL4QGk?si=bcgwkbuGHSZOk4oF" allowfullscreen></iframe>
+                    <h5 class="text-theme2">Dr. Navneet: Explosive Conversation - <font class="text-theme text-bold">Kasturi Jha</font></h5>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="container-fluid fh5co-news" id="community">
+        <div class="container">
+            <h2>Community & Impact</h2>
+            <p>At <strong>LOL "Let's Off Leash,"</strong> we believe in the power of storytelling to create meaningful change. Our growing community is a testament to the impact of our content, with countless listeners and viewers sharing how our stories have inspired them. From successful rescue missions to individuals taking action in their own communities, we are proud to foster a space where real change happens. We collaborate with organizations, animal welfare groups, and passionate individuals to extend our reach and make a difference. Want to be part of our movement? Share your story, support our mission, or join us in spreading awareness. Together, we can create a world where compassion and understanding thrive.</p>
+            <br>
+            <div class="row">
+                <div class="col-md-3">
+                    <i class="fa fa-comments"></i>
+                    <h5>Listener Stories</h5>
+                    <p>Our content has inspired people to adopt pets, support rescues, and make compassionate lifestyle changes.</p>
+                </div>
+
+                <div class="col-md-3">
+                    <i class="fa fa-handshake"></i>
+                    <h5>Collaborations & Partnerships</h5>
+                    <p>We partner with ethical brands and animal welfare organizations to amplify positive change.</p>
+                </div>
+
+                <div class="col-md-3">
+                    <i class="fa fa-users"></i>
+                    <h5>How to Get Involved</h5>
+                    <p>Support us by subscribing, sharing, volunteering, or contributing your own stories.</p>
+                </div>
+
+                <div class="col-md-3">
+                    <i class="fa fa-trophy"></i>
+                    <h5>Milestones & Achievements</h5>
+                    <p>From viral stories to esteemed guest features, we continue to inform, inspire, and ignite change.</p>
+                </div>
+            </div>
+            <br>
+            <hr>
+        </div>
+    </div>
+
+
+    <footer class="container-fluid fh5co-footer">
+        <div class="container" id="contact">
+            <div class="row">
+                <div class="col-lg-5">
+                    <h2>CONTACT US TODAY NOW</h2>
+                    <p class="light">
+                        Have a story to share or a question for us? <br>Reach out—we’d love to hear from you!
+                    </p>
+                    <p>
+                        <span class="email"><img src="{{URL::to('/')}}/public/images/email.png"
+                                alt="email icon" /></span><b><a href="mailto:askforkasturi@letsoffleash.com">askforkasturi@letsoffleash.com</a></b>
+                    </p>
+                    <!-- <p>
+                        <span class="phone"><img src="{{URL::to('/')}}/public/images/phone.png" alt="phone icon" /></span><b>+123-456-7890</b>
+                    </p> -->
+                    <h5 class="text-theme2">We Are Social:</h5>
+                    <ul class="navbar-nav float-left social-links footer-social">
+                       <li class="nav-item">
+                            <a class="nav-link" href="https://www.youtube.com/@LetsOffLeash" target="_blank" class="text-theme"><i class="fab fa-youtube"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://instagram.com/letsoffleash" target="_blank" class="text-theme"><i class="fab fa-instagram"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="http://facebook.com/LetsOffLeashlol" target="_blank" class="text-theme"><i class="fab fa-facebook-f"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://linkedin.com/company/letsoffleash" target="_blank" class="text-theme"><i class="fab fa-linkedin"></i></a>
+                        </li>
+
+                    </ul>
+                </div>
+
+                <div class="col-lg-7">
+                    <div class="form-box">
+                        <h4>What would you like to talk about</h4>
+                        <p>We'd Love to Hear From you !</p>
+                        <hr />
+                        <table class="table table-light table-borderless">
+                            <tr>
+                                <td><input type="text" class="form-control" placeholder="Name...">
+                                </td>
+
+                                <td><input type="text" class="form-control" placeholder="Email address">
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td colspan="2"><textarea class="form-control" placeholder="You Message"></textarea>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                    <button type="submit">
+                                        SUBMIT NOW
+                                    </button>
+
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </footer>
+    <div class="container-fluid copy">
+        <div class="col-lg-12">
+            <p>&copy; 2025 <a href="javascript:void(0)" class="text-theme2"><strong>LOL - Let's Off Leash</strong></a>. All rights Reserved.</p>
+        </div>
+    </div>
+
+    <script src="{{URL::to('/')}}/public/js/jquery.min.js"></script>
+    <script src="{{URL::to('/')}}/public/js/bootstrap.min.js"></script>
+    <script src="{{URL::to('/')}}/public/owl-carousel/owl.carousel.min.js"></script>
+    <script src="{{URL::to('/')}}/public/js/isotope-docs.min.js?6"></script>
+    <script src="{{URL::to('/')}}/public/js/main.js"></script>
+</body>
+
+</html>
