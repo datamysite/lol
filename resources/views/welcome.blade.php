2c4f22e31a9ca6d057647e3fad406fc8fa77d481
Waseem <waseem@datamysite.com>
2025-02-18 16:23:55 +0400

    og tags

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,9 +28,11 @@
         <nav class="navbar fixed-top navbar-expand-xl">
             <div class="container">
                 <a class="navbar-brand mobile-logo" href="#">
-                    <img src="{{URL::to('/')}}/public/images/kj-logo.png" alt="LOL" height="50px" />
+                    <img src="{{URL::to('/')}}/public/images/kj-logo.png" alt="LOL" height="45px" style="margin-top: 10px;" />
+                    &nbsp;&nbsp;|&nbsp;&nbsp;
+                    <img src="{{URL::to('/')}}/public/images/lol-logo.png" alt="LOL" height="55px" />
+                    &nbsp;&nbsp;|&nbsp;&nbsp;
                     <img src="{{URL::to('/')}}/public/images/lol-j-logo.png" alt="LOL" height="50px" />
-                    <img src="{{URL::to('/')}}/public/images/lol-logo.png" alt="LOL" height="50px" />
                 </a>
                 <button class="navbar-toggler" data-target="#my-nav" onclick="myFunction(this)" data-toggle="collapse">
                     <span class="bar1"></span> <span class="bar2"></span> <span class="bar3"></span> </button>
@@ -60,9 +62,9 @@
                         <li class="nav-item active">
                             <a class="nav-link" href="{{URL::to('/')}}">
                                 <img src="{{URL::to('/')}}/public/images/kj-logo.png" alt="LOL Logo" height="55px" />
-                                &nbsp;-&nbsp;
+                                &nbsp;|&nbsp;
                                 <img src="{{URL::to('/')}}/public/images/lol-logo.png" alt="LOL Logo" height="80px" />
-                                &nbsp;-&nbsp;
+                                &nbsp;|&nbsp;
                                 <img src="{{URL::to('/')}}/public/images/lol-j-logo.png" alt="LOL Logo" height="65px" />
                             </a>
                         </li>
